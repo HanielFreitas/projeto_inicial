2b216e525fd4e8179d66b7b4836a24ed4f4c6ae8
Haniel Freitas <hanielfreitas3@gmail.com>
2025-06-02 10:32:46 -0400

    Meu primeiro commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Jornad de dados")
+
+#Variáveis
+idade = 26
+nome = "Haniel Freitas"
+
+#Print
+print(idade)
+
+#Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+#Lista
+lista = [1, 2, 3, 4, 5]
+
+#Métodos
+lista_invertida = lista.reverse()
+print(lista_invertida)
